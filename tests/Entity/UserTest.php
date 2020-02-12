448182aea550b759c5de6d2d2d3c622363980496
fontaipi <pierrefontaine00@gmail.com>
2020-02-12 10:31:58 +0100

    user attribute nom et prenom + test nom prenom

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -1,10 +1,25 @@
 <?php
 
+use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
 final class UserTest extends TestCase
 {
     public function testNewUser(): void {
-        $this->assertInstanceOf(User::class, new User());
+        $user = new User();
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertNull($user->getId());
+    }
+
+    public  function testUserNom(): void {
+        $user = new User();
+        $user->setNom("martin");
+        $this->assertEquals("martin", $user->getNom());
+    }
+
+    public  function testUserPrenom(): void {
+        $user = new User();
+        $user->setPrenom("philippe");
+        $this->assertEquals("philippe", $user->getPrenom());
     }
 }
