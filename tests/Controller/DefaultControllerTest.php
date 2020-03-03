516ef75ed3cffb9ce0c0f0d4fb6942acd62ac6cc
fontaipi <pierrefontaine00@gmail.com>
2020-03-03 10:55:45 +0100

    modif homepage

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -7,11 +7,21 @@ class DefaultControllerTest extends WebTestCase
     public function testHomepage(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
         $this->assertSelectorExists('a[href="/login"]');
+        $this->assertSelectorExists('a[href="/register"]');
+
+    }
+
+    public function testHomepageuser(): void
+    {
+        $client = static::createClient([
+            'PHP_AUTH_USER' => 'username',
+            'PHP_AUTH_PW' => 'password'
+        ]);
+
         $this->assertSelectorExists('a[href="/logout"]');
 
     }
