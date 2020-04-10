0c4527207f72bc91e8a14fbba2a047f23c7fe502
QuentinReyn <57526153+QuentinReyn@users.noreply.github.com>
2020-04-10 13:37:23 +0200

    test to test if user logged can acceed to lieu/new

---
+++ tests/Controller/LieuControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Tests\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class LieuControllerTest extends WebTestCase
+{
+    public function testAllowedToUser(): void
+    {
+        $client = static::createClient(
+            [],
+            [
+                'PHP_AUTH_USER' => 'admin',
+                'PHP_AUTH_PW' => '123456',
+            ]
+        );
+        $crawler = $client->request('GET', '/lieu/new');
+        $this->assertNotEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+}
